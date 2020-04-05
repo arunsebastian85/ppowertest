a8eae435eac1d0f72ea6e1b37a79cf583857dd6f
sebastianarun <arun.sebastian@hmhco.com>
2020-04-05 21:48:33 +0100

    Committing tostring logic

--- src/main/java/com/ppower/reportgenerator/utils/ReportUtils.java
+++ src/main/java/com/ppower/reportgenerator/utils/ReportUtils.java
@@ -6,15 +6,16 @@ import java.util.Arrays;
 
 @Component
 public class ReportUtils {
-    private static final String SLCRHEADER= "Selection | Currency | NumOfBets | Total Stakes | Total Liability";
-    private static final String TLCRHEADER= "Currency | NumOfBets | Total Stakes | Total Liability";
+
+    private static final String SLCR_HEADER= "Selection | Currency | NumOfBets | Total Stakes | Total Liability";
+    private static final String TLCR_HEADER= "Currency | NumOfBets | Total Stakes | Total Liability";
     private static final String INPUT_FORMAT_ERROR_MSG="Invalid InputFormat param value. Allowed only [CSV,JSON] |";
     private static final String OUTPUT_FORMAT_ERROR_MSG="Invalid OutputFormat param value. Allowed only [CSV,CONSOLE] |";
     private static final String REPORT_TYPE_ERROR_MSG="Invalid ReportType param value. Allowed only [SLCReport,TLCReport]";
-
+    private static final String SLC_REPORT = "SLCReport";
 
     public String getReportHeader(String type){
-        return type.equalsIgnoreCase("SLCReport") ? SLCRHEADER : TLCRHEADER ;
+        return type.equalsIgnoreCase(SLC_REPORT) ? SLCR_HEADER : TLCR_HEADER ;
     }
 
     public String formatAmount( Float value){
@@ -23,21 +24,21 @@ public class ReportUtils {
 
     public void sanitizeInput(String inputFormat, String outputFormat, String reportType)
             throws RuntimeException{
-
         StringBuffer errorMessage = new StringBuffer();
-        if(!Arrays.stream(InputFormat.values()).anyMatch((t) -> t.name().equals(inputFormat))){
+        if(!Arrays.stream(InputFormat.values()).anyMatch((i) -> i.name().equals(inputFormat))){
             errorMessage.append(INPUT_FORMAT_ERROR_MSG);
         }
-        if(!Arrays.stream(OutputFormat.values()).anyMatch((t) -> t.name().equals(outputFormat))){
+        if(!Arrays.stream(OutputFormat.values()).anyMatch((o) -> o.name().equals(outputFormat))){
             errorMessage.append(OUTPUT_FORMAT_ERROR_MSG);
         }
-        if(!Arrays.stream(ReportType.values()).anyMatch((t) -> t.name().equals(reportType))) {
+        if(!Arrays.stream(ReportType.values()).anyMatch((r) -> r.name().equals(reportType))) {
             errorMessage.append(REPORT_TYPE_ERROR_MSG);
         }
         if(!errorMessage.toString().isEmpty()){
             throw new RuntimeException(errorMessage.toString());
         }
     }
+
 }
 
 enum InputFormat {
